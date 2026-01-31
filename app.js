885f815c1ee2511245d2585d781abed3069401e1
arjoov <vishwakarmaarjoo@gmail.com>
2026-01-31 22:37:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Its Arjoo Vishwakarma and this is the first CI/CD Application!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
